5a6d3d1661c27ce2e858d70625664726d3736d33
MathisDupriez <la217548@student.helha.be>
2024-04-21 23:11:09 +0200

    Changement du port pour debian

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 
 const app = express();
-const port = 3000;
+const port = 54404;
 
 app.use(express.static('public'));
 app.use(express.static('image'));
