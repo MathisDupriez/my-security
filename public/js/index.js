beee0e76d8308b6e83573a4dcf80790b7ebb4658
MathisDupriez <la217548@student.helha.be>
2024-09-01 23:44:07 +0200

    correctif pour fit à la nouvel déa

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 // Sélection de l'élément template et de l'endroit où les articles seront ajoutés
 var articleTemplate = document.getElementById('articleTemplate');
 const index = document.getElementById('article');
+
 // Fonction asynchrone pour vérifier si une image existe
 async function imageExists(url) {
     return new Promise((resolve) => {
@@ -12,28 +13,28 @@ async function imageExists(url) {
 }
 
 // Fonction pour ajouter un nouvel article à la page
-async function addArticle(articleTitle, articleContent,articleDate,articleLikes,articleImage,articleId) {
+async function addArticle(articleTitle, articleContent, articleDate, articleLikes, articleImage, articleId) {
     const clone = document.importNode(articleTemplate.content, true);
     clone.getElementById('articleTitle').textContent = articleTitle || 'Article Title';
     clone.getElementById('articleContent').textContent = articleContent || 'Article Content';
     clone.getElementById('articleDate').textContent = articleDate || 'Date inconnue';
+    
     // Vérifier si l'image existe
     const exists = await imageExists(articleImage);
-    console.log(articleId);
     if (exists) {
         clone.getElementById('articleImage').src = articleImage;
     } else {
         clone.getElementById('articleImage').src = './image/no-image.png'; // Utilisation d'une image par défaut si l'image n'existe pas
     }
+
     clone.getElementById("articleButton").addEventListener("click", function() {
-        //redirect to article page
+        // Redirection vers la page de l'article
         const id = articleId || '1';
         window.location.href = `/articles/${id}`;
     });
+
     index.appendChild(clone);
 }
-
-
 
 // Fonction asynchrone pour récupérer les articles depuis le serveur
 async function fetchArticles() {
@@ -53,6 +54,10 @@ async function fetchArticles() {
 // Fonction principale asynchrone pour charger les articles
 async function loadArticles() {
     const articles = await fetchArticles();
+    
+    // Trier les articles par date de la plus récente à la plus ancienne
+    articles.sort((a, b) => new Date(b.Date) - new Date(a.Date));
+    
     // Parcours des articles et ajout à la page
     articles.forEach(article => {
         addArticle(article.Title, article.Description, article.Date, article.Likes, article.ImagePath, article.ID);
@@ -61,13 +66,13 @@ async function loadArticles() {
 
 (async () => {
     try {
-        // Récupérer les image du serveur Express
+        // Récupérer les images du serveur Express
         const response = await fetch('/images');
         const images = await response.json();
 
         const carouselInner = document.querySelector('.carousel-inner');
 
-        // Parcourir la liste des image et créer les éléments correspondants
+        // Parcourir la liste des images et créer les éléments correspondants
         for (const image of images) {
             const index1 = images.indexOf(image);
             const carouselItem = document.createElement('div');
@@ -81,7 +86,7 @@ async function loadArticles() {
             if (exists) {
                 img.src = '/image/' + image;
             } else {
-                img.src= '/image/no-image.png'; // Utilisation d'une image par défaut si l'image n'existe pas
+                img.src = '/image/no-image.png'; // Utilisation d'une image par défaut si l'image n'existe pas
             }
             img.classList.add('d-block', 'w-100');
             img.alt = 'Image ' + index1;
@@ -96,4 +101,3 @@ async function loadArticles() {
 
 // Appel de la fonction principale pour charger les articles
 loadArticles();
-
